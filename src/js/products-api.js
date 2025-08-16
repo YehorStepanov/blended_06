6255167c44fd0e88ee3fc46e331241137bce7246
Vitalii Shumakov <fafde8120@gmail.com>
2025-08-16 19:01:29 +0200

    task2

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -5,3 +5,11 @@ export async function getCategory() {
     const answer = await axios('/products/category-list');
     return answer.data;
 }
+
+
+export async function getProducts(currentPage = 1) {
+    
+const url = `/products?limit=12&skip=${(currentPage - 1) * 12}`;
+const answer = await axios(url);
+    return answer.data.products;
+}
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  categories: document.querySelector(".categories "),
+  products: document.querySelector(".products"),
+};
+
